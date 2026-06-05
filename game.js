08acc85a63b9889a9ea5fe74bcf3ba3c35f764a2
squishy899 <nathancooper899@gmail.com>
2026-06-05 00:26:05 -0400

    game.js

--- game.js
+++ game.js
@@ -94,14 +94,6 @@ function gameLoop(){
 
     scrap += getSPS()/10;
     
-let power = 0;
-let food = 50;
-let technology = 0;
-
-let generators = 0;
-let gardens = 0;
-let labs = 0;
-
     updateUI();
 }
 
