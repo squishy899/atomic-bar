2b3d7e402c0ef75a8f4c3bcbcb82d56451ab936c
squishy899 <nathancooper899@gmail.com>
2026-06-04 23:19:29 -0400

    save.js

--- save.js
+++ save.js
@@ -5,12 +5,28 @@ function saveGame(){
         JSON.stringify({
             scrap,
             survivors,
+
+            power,
+            food,
+            technology,
+
             scrapHeaps,
             scavengerTeams,
+
+            generators,
+            gardens,
+            labs,
+
             heapCost,
             teamCost,
+
+            generatorCost,
+            gardenCost,
+            labCost,
+
             lastSave: Date.now()
         })
+
     );
 }
 
@@ -26,11 +42,23 @@ function loadGame(){
     scrap = save.scrap;
     survivors = save.survivors;
 
+    power = save.power || 0;
+    food = save.food || 50;
+    technology = save.technology || 0;
+
     scrapHeaps = save.scrapHeaps;
     scavengerTeams = save.scavengerTeams;
 
+    generators = save.generators || 0;
+    gardens = save.gardens || 0;
+    labs = save.labs || 0;
+
     heapCost = save.heapCost;
     teamCost = save.teamCost;
+
+    generatorCost = save.generatorCost || 100;
+    gardenCost = save.gardenCost || 75;
+    labCost = save.labCost || 250;
 
     let secondsAway =
         (Date.now() - save.lastSave)/1000;
